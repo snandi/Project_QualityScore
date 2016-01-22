f7843052d619d2cf7e3ccd087880d1dc2218c8bd
snandi <nandi@stat.wisc.edu>
2016-01-22 13:43:09 -0600

    Updated molecule list python script

--- PythonScripts_QualityScore/produce_molelist.py
+++ PythonScripts_QualityScore/produce_molelist.py
@@ -32,8 +32,8 @@ from os import path
 
 ## global variables for mm52_all7341
 Input_inforParentFolder = '/exports/aspen/nandi/mm52-all7341/alignmentChunks/'
-Output_ListParentFolder = '/aspen/nandi/Project_QualityScore/Data/mm52-all7341/chr7/'
-InforFileName = 'alignmentChunks.withLength.all7134Groups.goldOnly_chr7'
+#Output_ListParentFolder = '/aspen/nandi/Project_QualityScore/Data/mm52-all7341/chr7/'
+#InforFileName = 'alignmentChunks.withLength.all7134Groups.goldOnly_chr7'
 
 #######################################################################################
 ## main: get the molecule list file given interval number (and Chromosome number)
@@ -49,6 +49,9 @@ def main(argv):
     	IntervalNum = int(args[0])
 	ChrNum = int(args[1]) 
 	#ChrNum = 1
+
+	Output_ListParentFolder = '/aspen/nandi/Project_QualityScore/Data/mm52-all7341/chr' + str(ChrNum) +'/'
+	InforFileName = 'alignmentChunks.withLength.all7134Groups.goldOnly_chr' + str(ChrNum)
 
 	ListFileName = 'Chr' + str(ChrNum) + '_interval' + str(IntervalNum) + '.txt'
 	InforReadPath = Input_inforParentFolder + InforFileName
